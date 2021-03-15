0068625546c7c6b44810e3e23fca0beefd054a64
harisdanial97 <72817250+harisdanial97@users.noreply.github.com>
2021-03-15 16:40:17 +0000

    Update JS_10.js

--- JS_10.js
+++ JS_10.js
@@ -55,6 +55,7 @@ jQuery(document).ready(function() {
             document.getElementById("ninth").style.visibility = "hidden";
             document.getElementById("Assumptions1").value = "s2";
             jQuery("#Assumptions1").css( 'border', '');
+            jQuery(".final").hide();jQuery("#nineteenth").hide();
 
             if((jQuery('#third').attr('data-click-state') == 1) && (jQuery('#fourth').attr('data-click-state') == 1) && (jQuery('#sixth').attr('data-click-state') == 1) ) {
                 jQuery('#eighth').html("\\({{\\partial \\rho} \\over {\\partial t}} + {{\\partial u} \\over {\\partial x}}  + {{\\partial v} \\over {\\partial y}} + {{\\partial w} \\over {\\partial z}}  = 0 \\vphantom{\\require{cancel}\\cancelto{0} {{\\partial \\rho} \\over {\\partial t}}}\\)");
@@ -154,6 +155,8 @@ jQuery('#third').on('click', function() {
         document.getElementById("myform2").style.visibility = "hidden";
         document.getElementById("tenth").style.visibility = "hidden";
         document.getElementById("Assumptions2").value = "s2";
+        jQuery("#Assumptions2").css( 'border', '');
+        jQuery(".final").hide();jQuery("#nineteenth").hide();
 
         if((jQuery('#first').attr('data-click-state') == 1) && (jQuery('#fourth').attr('data-click-state') == 1) && (jQuery('#sixth').attr('data-click-state') == 1) ) {
             jQuery('#eighth').html("\\({{\\partial \\rho} \\over {\\partial t}} + {{\\partial u} \\over {\\partial x}}  + {{\\partial v} \\over {\\partial y}} + {{\\partial w} \\over {\\partial z}}  = 0 \\vphantom{\\require{cancel}\\cancelto{0} {{\\partial \\rho} \\over {\\partial t}}}\\)");
@@ -255,6 +258,7 @@ jQuery('#fourth').on('click', function() {
         document.getElementById("myform3").style.visibility = "hidden";
         document.getElementById("eleventh").style.visibility = "hidden";
         document.getElementById("Assumptions3").value = "s2";
+        jQuery("#Assumptions3").css( 'border', '');
 
 
         if((jQuery('#first').attr('data-click-state') == 1) && (jQuery('#third').attr('data-click-state') == 1) && (jQuery('#sixth').attr('data-click-state') == 1) ) {
@@ -356,6 +360,8 @@ jQuery('#sixth').on('click', function() {
         document.getElementById("myform4").style.visibility = "hidden";
         document.getElementById("twelfth").style.visibility = "hidden";
         document.getElementById("Assumptions4").value = "s2";
+        jQuery("#Assumptions4").css( 'border', '');
+        jQuery(".final").hide();jQuery("#nineteenth").hide();
 
         if((jQuery('#first').attr('data-click-state') == 1) && (jQuery('#third').attr('data-click-state') == 1) && (jQuery('#fourth').attr('data-click-state') == 1) ) {
             jQuery('#eighth').html("\\({{\\partial \\rho} \\over {\\partial t}} + {{\\partial u} \\over {\\partial x}}  + {{\\partial v} \\over {\\partial y}} + {{\\partial w} \\over {\\partial z}}  = 0 \\vphantom{\\require{cancel}\\cancelto{0} {{\\partial \\rho} \\over {\\partial t}}}\\)");
@@ -413,91 +419,28 @@ jQuery("#Assumptions1").select().on("change", function() {
   var answer = document.getElementById("Assumptions1");
   document.getElementById("ninth").style.visibility = "hidden";
   jQuery("#Assumptions1").css( 'border', '');
-  if (answer.value == "a1") {
-   
-	if(Assumptions1_check == 0){
-		first_section += 1;
-		Assumptions1_check = 1;
-	}
-	
-} else {
-    
-    
-	if(Assumptions1_check == 1){
-		first_section -= 1;
-		Assumptions1_check = 0;
-	}
-  }
-
-  
-  
 });
 
 jQuery("#Assumptions2").select().on("change", function() {
   var answer = document.getElementById("Assumptions2");
   document.getElementById("tenth").style.visibility = "hidden";
-  jQuery("#Assumptions2").css( 'border', '');
-  if (answer.value == "b2") {
-	
-	if(Assumptions2_check == 0){
-		first_section += 1;
-		Assumptions2_check = 1;
-	}
-} else {
-    
-	
-	if(Assumptions2_check == 1){
-		first_section -= 1;
-		Assumptions2_check = 0;
-	}
-  }
- 
+  jQuery("#Assumptions2").css( 'border', ''); 
 });
 
 jQuery("#Assumptions3").select().on("change", function() {
   var answer = document.getElementById("Assumptions3");
   document.getElementById("eleventh").style.visibility = "hidden";
   jQuery("#Assumptions3").css( 'border', '');
-  if (answer.value == "s2") {
-    
-	
-	if(Assumptions3_check == 0){
-		first_section += 0;
-		Assumptions3_check = 1;
-	}
-	
-} else {
-    
-	
-	if(Assumptions3_check == 1){
-		first_section -= 0;
-		Assumptions3_check = 0;
-	}
-  }
+
  
 });
 
 
 jQuery("#Assumptions4").select().on("change", function() {
-	
 document.getElementById("twelfth").style.visibility = "hidden";
 jQuery("#Assumptions4").css( 'border', '');
-  var answer = document.getElementById("Assumptions4");
-  if (answer.value == "d2") {
-    
-	if(Assumptions4_check == 0){
-		first_section += 1;
-		Assumptions4_check = 1;
-	}
-	
-} else {
-    
-	
-	if(Assumptions4_check == 1){
-		first_section -= 1;
-		Assumptions4_check = 0;
-	}
-  }
+var answer = document.getElementById("Assumptions4");
+
   
 });
 
@@ -620,7 +563,10 @@ function myFunction4() {
 setTimeout(function(){ 
 
 $('.ans_btn').click(function() {
-	if(first_section == 4){
+    var answer1 = document.getElementById("Assumptions1");
+    var answer2 = document.getElementById("Assumptions2"); 
+    var answer4 = document.getElementById("Assumptions4"); 
+	if((answer1.value =="a1") && (answer2.value =="b2") && (jQuery('#fourth').attr('data-click-state') == 1) && (answer4.value =="d2") ){
 		jQuery("#nineteenth").show();
 		 jQuery(this).css('border', '2px solid green');
 		 jQuery('.btn_icon').html('<span><img src="https://whytowiki.com/scion/public/check.svg" class="icon_img"/></span>  <span class="tooltip__content"><p class="tooltip_header">Correct <span class="tooltip_img"><img src="https://whytowiki.com/scion/public/check.svg" class="icon_img"/></span></p><p class="contant_tooltip"><b>Correct Answer:</b> Cancel 1st, 2nd & 4th term. Then, attribute the correct assumptions</p></span>');
