07e1a2e373ff6f9bf60466e6590aad6329f467ed
harisdanial97 <72817250+harisdanial97@users.noreply.github.com>
2021-05-16 21:38:13 +0100

    Update JS_18.js

--- JS_18.js
+++ JS_18.js
@@ -1098,7 +1098,7 @@ $('.ans_btn').click(function() {
          frame = 2;
          jQuery("#eighth").css('border', '2px solid green');
          jQuery("#eighth").css('background-color', '#90EE90');
-         jQuery('.btn_icon').html('<span><img src="https://whytowiki.com/scion/public/check.svg" class="icon_img"/></span>  <span class="tooltip__content"><p class="tooltip_header">Correct <span class="tooltip_img"><img src="https://whytowiki.com/scion/public/check.svg" class="icon_img"/></span></p><p class="contant_tooltip"><b>Correct Answer:</b> Please alter your answers above. Cancel the 1st, 2nd & 4th term, then, attribute the correct assumptions respectively</p></span>');
+         jQuery('.btn_icon').html('<span><img src="https://whytowiki.com/scion/public/check.svg" class="icon_img"/></span>  <span class="tooltip__content"><p class="tooltip_header">Correct <span class="tooltip_img"><img src="https://whytowiki.com/scion/public/check.svg" class="icon_img"/></span></p><p class="contant_tooltip"><b>Correct Answer:</b> Cancel the 1st, 2nd & 4th term. Then, attribute the correct assumptions respectively</p></span>');
          jQuery('.btn_icon').css('visibility', 'visible');
          jQuery('#ans_btn').css('visibility', 'hidden');
          
